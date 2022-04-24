0438e9cfe21f3e4c18130a0675e40bc38a6d480b
Christopher Eglinton <christopher.t.eglinton@gmail.com>
2022-04-24 18:19:52 +0200

    upload

---
+++ src/components/Cards.jsx
@@ -0,0 +1,53 @@
+import './Cards.css'
+import bjj from '../img/bjj.jpg'
+import data_analysis from '../img/data_analysis.png'
+import web_dev from '../img/web_development.png'
+
+const Cards = () => {
+    return (
+        <div className={"cards"}>
+
+                <div className="col-4">
+                    <div className={'card'}>
+                           <div className={'card-title'}>
+                                <h1>Data Analysis</h1>
+                           </div>
+                         <img src={data_analysis}/>
+                         <div className={'card-body'}>
+                            Lorem upsum
+                            Lorem upsum
+                            Lorem upsum
+                        </div>
+                    </div>
+                </div>
+                   <div className="col-4">
+                    <div className={'card'}>
+                           <div className={'card-title'}>
+                               <h1>Web Development</h1>
+                           </div>
+                         <img src={web_dev}/>
+                         <div className={'card-body'}>
+                            Lorem upsum
+                            Lorem upsum
+                            Lorem upsum
+                        </div>
+                    </div>
+                </div>
+                   <div className="col-4">
+                    <div className={'card'}>
+                        <div className={'card-title'}>
+                            <h1>Athlete</h1>
+                        </div>
+                             <img src={bjj}/>
+                        <div className={'card-body'}>
+                            Lorem upsum
+                            Lorem upsum
+                            Lorem upsum
+                        </div>
+                    </div>
+                </div>
+        </div>
+    )
+}
+
+export default Cards
---
+++ src/components/Intro.jsx
@@ -0,0 +1,36 @@
+import './Intro.css'
+
+const Intro = () => {
+    return (
+         <div className={'i'}>
+
+            <div className={'i-left'}>
+
+                <div className={'i-left-wrapper'}>
+                          <div className={'i-bg'}>
+
+                          </div>
+                    <h2 className={'i-intro'}>Hello, I'm</h2>
+                    <h2 className={'i-name'}>Christopher Eglinton</h2>
+                    <div className={'i-title'}>
+                        <div className={'i-title-wrapper'}>
+                            <div className={'i-title-item'}>Data Analyst</div>
+                            <div className={'i-title-item'}>Web Developer</div>
+                            <div className={'i-title-item'}>Athlete</div>
+                            <div className={'i-title-item'}>Academic</div>
+                            <div className={'i-title-item'}>Cool dude &#128526;</div>
+                        </div>
+                    </div>
+                    <div className={'i-intro'}>
+                        Everything from my analysis, web development, studies and even training - is dictated by data driven decision making. I believe in taking an extremely technical approach to each challenge I come across.
+                    </div>
+                </div>
+             </div>
+             <div className={'i-right'}>
+                right
+             </div>
+        </div>
+    )
+}
+
+export default Intro
